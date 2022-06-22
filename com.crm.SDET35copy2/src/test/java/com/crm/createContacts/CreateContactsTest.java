5d5f52a02a93edf47507cc110d0b56959828d622
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-06-22 13:17:23 +0530

    commit changes

--- com.crm.SDET35copy2/src/test/java/com/crm/createContacts/CreateContactsTest.java
+++ com.crm.SDET35copy2/src/test/java/com/crm/createContacts/CreateContactsTest.java
@@ -14,7 +14,7 @@ public class CreateContactsTest extends BaseClass{
 @Test(retryAnalyzer = com.crm.genericUtilities.RetryAnalyserImptn.class)
 	public void createContactTest() throws Throwable{
 		
-		//Hi
+		//Hiiiiii
 		//To get a random number
 		int randNum = jLib.getRandomNumber();
 		
