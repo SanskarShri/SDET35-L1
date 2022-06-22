0588bb9eacc94576660988c5f9eeede09c1b05e0
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-06-22 15:20:21 +0530

    commit

--- com.crm.SDET35copy2/src/test/java/com/crm/createOrganisation/ToCreateOrganisationTest.java
+++ com.crm.SDET35copy2/src/test/java/com/crm/createOrganisation/ToCreateOrganisationTest.java
@@ -17,6 +17,7 @@ public class ToCreateOrganisationTest extends BaseClass{
 @Test(retryAnalyzer = com.crm.genericUtilities.RetryAnalyserImptn.class)
 public void createOrganisationTest() throws Throwable{
 	
+	//Hi
 	//To get random number
 	int randNum = jLib.getRandomNumber();
 	
